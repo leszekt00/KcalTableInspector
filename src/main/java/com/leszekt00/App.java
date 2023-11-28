db0430449c374ad0626a309e4b45c8075479caff
leszekt00 <leszekt00@gmail.com>
2023-11-28 01:30:22 -0800

    Added basic image loading and reading text using tess4j

--- src/main/java/com/leszekt00/App.java
+++ src/main/java/com/leszekt00/App.java
@@ -1,13 +1,21 @@
 package com.leszekt00;
 
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //new ImageParser("blabla");
+        new ImageParser("resources/sample.png");
+
     }
 }
---
+++ src/main/java/com/leszekt00/ImageParser.java
@@ -0,0 +1,54 @@
+package com.leszekt00;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+
+public class ImageParser {// given a loaded image parse its contents into variables
+    private BufferedImage image;
+    private String filePath;
+    public ImageParser(String filePath) { // save filepath and load the image from it
+        this.filePath = filePath;
+        try {
+            this.image = loadImage(filePath);
+        } catch (RuntimeException e) {
+            throw e;
+        }
+        readTextFromImage(image);
+
+    }
+    private BufferedImage loadImage (String filePath) { // loading image into BufferedImage
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            // Load the image from the FileInputStream
+            return ImageIO.read(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Cannot find file with the path:"+filePath,e);
+        }
+    }
+
+    private String readTextFromImage(BufferedImage im) { // reads text from image using Tess4j
+        StringBuilder result = new StringBuilder("image content:")
+                .append(System.lineSeparator());
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+        instance.setDatapath("Tess4j/tessdata"); // path to tessdata directory
+
+        try {
+            result.append(instance.doOCR(im));
+            System.out.println(result);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+
+}
